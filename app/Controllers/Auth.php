52e64ac3a8db6f1834141203b8f9cf74f03f7bb8
Cesar Zelaya <cesarzelayabk@hotmail.com>
2020-12-02 20:46:58 -0600

    token time

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -5,6 +5,10 @@ class Auth extends BaseController
 {
     use ResponseTrair;
 
+    public function __construct(){
+        helper('secure_password');
+    }
+
 	public function login()
 	{
 		try {
@@ -12,14 +16,39 @@ class Auth extends BaseController
             $password = $this->request->getPost('password');
 
             $usuarioModel = new UsuarioModel();
-            $where = ['username'=>$username, 'password'=>$password];
+            //$where = ['username'=>$username, 'password'=>$password];
+            $validateUsuario = $usuarioModel->where('username', $username)->first();
 
-            $validateUsuario = $usuarioModel->where($where)->find();
+            if ($validateUsuario == null) {
+                return $this->failNotFound('Usuario no encontrado');
+
+                if(verifyPassword($password, $validateUsuario["password"])):
+                    
+                    $jwt = $this->generateJWT($validateUsuario);
+                    return $this->respond($jwt);
+
+                else:
+                    return $this->failValidationError('Contraseña invalida');
+                endif;
+
+             //   return $this->respond('Usuario encontrado');
+            }
 
         } catch (\Exception $e) {
             return $this->failServerError('Ha ocurrido un error en el servidor');
         }
 	}
 
-	
+	protected function generateJWT($usuario){
+        $key = Services::getSecretKey();
+        $time = time();
+        $payload=[
+            'aud'=>base_url(),
+            'iat'=> $time,
+            'exp'=> $time + 120,
+        ];
+
+        $jwt = JWT::encode($payload, $key);
+        return $jwt;
+    }
 }
