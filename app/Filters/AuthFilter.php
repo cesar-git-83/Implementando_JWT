fc36b2b0af31618214614c272cc0a14134ab0e18
Cesar Zelaya <cesarzelayabk@hotmail.com>
2020-12-05 10:50:10 -0600

    roles access

--- app/Filters/AuthFilter.php
+++ app/Filters/AuthFilter.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Filters;
 
+use App\Models\RolModel;
 use CodeIgniter\Filters\FilterInterface;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
@@ -24,7 +25,14 @@ class AuthFilter implements FilterInterface{
                 $arr = explode('', $authHeader);
                 $jwt = $arr[1];
 
-                JWT::decode($jwt, $key, ['HS256']);
+                $jwt = JWT::decode($jwt, $key, ['HS256']);
+
+                $rolModel = new RolModel();
+                $rol = $rolModel->find($jwt->data->rol);
+                if ($rol == null) 
+                return Services::response()->setStatusCode(ResponseInterface::HTTP_UNAUTHORIZED, 'El rol del JWT es invalido');
+
+                return true;
 
             }           
 
